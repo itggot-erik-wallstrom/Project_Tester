831e2acb8a2d598776253d4bbc5e4740a1643d60
itggot-erik-wallstrom <erik.wallstrom@itggot.se>
2016-11-23 08:01:55 +0100

    Last patch

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install gcc pkg-config ruby gem libgtk-3-dev libvte-2.91-dev
+sudo apt install make gcc pkg-config ruby gem libgtk-3-dev libvte-2.91-dev
 sudo gem install colorize
 
 make
